1728754b6ecdb5163017a7f4db957911a5fd1275
nekosu <liao.hy@outlook.com>
2024-07-17 11:10:47 +0800

    refactor: pi

--- src/projectInterface/launcher.ts
+++ src/projectInterface/launcher.ts
@@ -5,7 +5,6 @@ import { commands } from '../command'
 import { Service } from '../data'
 import { PipelineProjectInterfaceProvider } from '../pipeline/pi'
 import { PipelineRootStatusProvider } from '../pipeline/root'
-import { currentWorkspace, exists } from '../utils/fs'
 import {
   configController,
   configTask,
@@ -70,183 +69,6 @@ function overwriteTKSConfig(
   const base_key = (base >> 8) & 0xff
   const base_screencap = (base >> 16) & 0xff
   return (touch ?? base_touch) | ((key ?? base_key) << 8) | ((screencap ?? base_screencap) << 16)
-}
-
-export async function prepareRuntime(
-  data: Interface,
-  config: InterfaceConfig,
-  root: string // overwrite PROJECT_DIR
-): Promise<InterfaceRuntime | null> {
-  const result: Partial<InterfaceRuntime> = {}
-
-  const ctrlInfo = data.controller.find(x => x.name === config.controller.name)
-
-  if (!ctrlInfo) {
-    await vscode.window.showErrorMessage(
-      vscode.l10n.t('maa.pi.error.cannot-find-controller', config.controller.name)
-    )
-    return null
-  }
-
-  if (ctrlInfo.type === 'Adb') {
-    if (!config.adb) {
-      await vscode.window.showErrorMessage(
-        vscode.l10n.t('maa.pi.error.cannot-find-adb-for-controller', config.controller.name)
-      )
-      return null
-    }
-
-    result.controller_param = {
-      ctype: 'adb',
-      adb_path: config.adb.adb_path,
-      adb_serial: config.adb.address,
-      adb_config: JSON.stringify(ctrlInfo.adb?.config ?? config.adb.config),
-      adb_controller_type: overwriteTKSConfig(
-        maa.AdbControllerType.Input_Preset_AutoDetect |
-          maa.AdbControllerType.Screencap_FastestLosslessWay,
-        ctrlInfo.adb?.touch ?? null,
-        ctrlInfo.adb?.key ?? null,
-        ctrlInfo.adb?.screencap ?? null
-      )
-    }
-  } else if (ctrlInfo.type === 'Win32') {
-    if (!config.win32) {
-      await vscode.window.showErrorMessage(
-        vscode.l10n.t('maa.pi.error.cannot-find-win32-for-controller', config.controller.name)
-      )
-      return null
-    }
-
-    if (!config.win32.hwnd) {
-      await vscode.window.showErrorMessage(
-        vscode.l10n.t('maa.pi.error.cannot-find-hwnd-for-controller', config.controller.name)
-      )
-      return null
-    }
-
-    result.controller_param = {
-      ctype: 'win32',
-      hwnd: config.win32.hwnd,
-      controller_type: overwriteTKSConfig(
-        maa.Win32ControllerType.Touch_Seize |
-          maa.Win32ControllerType.Key_Seize |
-          maa.Win32ControllerType.Screencap_DXGI_DesktopDup,
-        ctrlInfo.win32?.touch ?? null,
-        ctrlInfo.win32?.key ?? null,
-        ctrlInfo.win32?.screencap ?? null
-      )
-    }
-  }
-
-  const resInfo = data.resource.find(x => x.name === config.resource)
-
-  if (!resInfo) {
-    await vscode.window.showErrorMessage(
-      vscode.l10n.t('maa.pi.error.cannot-find-resource', config.resource)
-    )
-    return null
-  }
-
-  result.resource_path = resInfo.path.map(x => x.replace('{PROJECT_DIR}', root))
-
-  result.task = []
-  for (const task of config.task) {
-    const taskInfo = data.task.find(x => x.name === task.name)
-
-    if (!taskInfo) {
-      await vscode.window.showErrorMessage(
-        vscode.l10n.t('maa.pi.error.cannot-find-task', task.name)
-      )
-      return null
-    }
-
-    const param: {} = {}
-
-    // 是不是该检查一下task里面指定的option是否都被配置了？如果缺失的话看看要不要读下default
-    for (const opt of task.option ?? []) {
-      const optInfo = data.option?.[opt.name]
-
-      if (!optInfo) {
-        await vscode.window.showErrorMessage(
-          vscode.l10n.t('maa.pi.error.cannot-find-option', opt.name)
-        )
-        return null
-      }
-
-      const csInfo = optInfo.cases.find(x => x.name === opt.value)
-
-      if (!csInfo) {
-        await vscode.window.showErrorMessage(
-          vscode.l10n.t('maa.pi.error.cannot-find-case-for-option', opt.value, opt.name)
-        )
-        return null
-      }
-
-      Object.assign(param, csInfo.param ?? {})
-    }
-
-    // 看看FW代码里面这玩意在前面还是后面
-    Object.assign(param, taskInfo.param ?? {})
-
-    result.task.push({
-      name: task.name,
-      entry: taskInfo.entry,
-      param: param
-    })
-  }
-
-  return result as InterfaceRuntime
-}
-
-export async function launchRuntime(runtime: InterfaceRuntime, output: vscode.OutputChannel) {
-  let controller: maa.ControllerBase
-
-  if (runtime.controller_param.ctype === 'adb') {
-    controller = new maa.AdbController(runtime.controller_param)
-  } else if (runtime.controller_param.ctype === 'win32') {
-    controller = new maa.Win32Controller(
-      runtime.controller_param.hwnd,
-      runtime.controller_param.controller_type
-    )
-  } else {
-    return
-  }
-
-  controller.notify = (msg, detail) => {
-    output.append(`${msg} ${detail}\n`)
-  }
-
-  await controller.post_connection()
-
-  const resource = new maa.Resource()
-
-  resource.notify = (msg, detail) => {
-    output.append(`${msg} ${detail}\n`)
-  }
-
-  for (const path of runtime.resource_path) {
-    await resource.post_path(path)
-  }
-
-  const instance = new maa.Instance()
-
-  instance.notify = (msg, detail) => {
-    output.append(`${msg} ${detail}\n`)
-  }
-
-  instance.bind(controller)
-  instance.bind(resource)
-
-  if (!instance.inited) {
-    instance.destroy()
-    controller.destroy()
-    resource.destroy()
-    return
-  }
-
-  for (const task of runtime.task) {
-    await instance.post_task(task.entry, task.param as unknown as maa.PipelineDecl).wait()
-  }
 }
 
 export class ProjectInterfaceLaunchProvider extends Service {
@@ -391,9 +213,7 @@ export class ProjectInterfaceLaunchProvider extends Service {
           {
             label: vscode.l10n.t('maa.pi.entry.launch'),
             action: async () => {
-              const runtime = await prepareRuntime(
-                pip.interfaceJson!,
-                pip.interfaceConfigJson!,
+              const runtime = await this.prepareRuntime(
                 this.shared(PipelineRootStatusProvider).activateResource!.dirUri.fsPath
               )
 
@@ -402,7 +222,7 @@ export class ProjectInterfaceLaunchProvider extends Service {
               if (runtime) {
                 this.outputChannel.show(true)
                 try {
-                  await launchRuntime(runtime, this.outputChannel)
+                  await this.launchRuntime(runtime)
                 } catch (err) {
                   this.outputChannel.append(`${err}\n`)
                 }
@@ -426,5 +246,188 @@ export class ProjectInterfaceLaunchProvider extends Service {
         }
       }
     })
+  }
+
+  async prepareRuntime(
+    root: string // overwrite PROJECT_DIR
+  ): Promise<InterfaceRuntime | null> {
+    const pip = this.shared(PipelineProjectInterfaceProvider)
+    const data = pip.interfaceJson
+    const config = pip.interfaceConfigJson
+
+    if (!data || !config) {
+      return null
+    }
+
+    const result: Partial<InterfaceRuntime> = {}
+
+    const ctrlInfo = data.controller.find(x => x.name === config.controller.name)
+
+    if (!ctrlInfo) {
+      await vscode.window.showErrorMessage(
+        vscode.l10n.t('maa.pi.error.cannot-find-controller', config.controller.name)
+      )
+      return null
+    }
+
+    if (ctrlInfo.type === 'Adb') {
+      if (!config.adb) {
+        await vscode.window.showErrorMessage(
+          vscode.l10n.t('maa.pi.error.cannot-find-adb-for-controller', config.controller.name)
+        )
+        return null
+      }
+
+      result.controller_param = {
+        ctype: 'adb',
+        adb_path: config.adb.adb_path,
+        adb_serial: config.adb.address,
+        adb_config: JSON.stringify(ctrlInfo.adb?.config ?? config.adb.config),
+        adb_controller_type: overwriteTKSConfig(
+          maa.AdbControllerType.Input_Preset_AutoDetect |
+            maa.AdbControllerType.Screencap_FastestLosslessWay,
+          ctrlInfo.adb?.touch ?? null,
+          ctrlInfo.adb?.key ?? null,
+          ctrlInfo.adb?.screencap ?? null
+        )
+      }
+    } else if (ctrlInfo.type === 'Win32') {
+      if (!config.win32) {
+        await vscode.window.showErrorMessage(
+          vscode.l10n.t('maa.pi.error.cannot-find-win32-for-controller', config.controller.name)
+        )
+        return null
+      }
+
+      if (!config.win32.hwnd) {
+        await vscode.window.showErrorMessage(
+          vscode.l10n.t('maa.pi.error.cannot-find-hwnd-for-controller', config.controller.name)
+        )
+        return null
+      }
+
+      result.controller_param = {
+        ctype: 'win32',
+        hwnd: config.win32.hwnd,
+        controller_type: overwriteTKSConfig(
+          maa.Win32ControllerType.Touch_Seize |
+            maa.Win32ControllerType.Key_Seize |
+            maa.Win32ControllerType.Screencap_DXGI_DesktopDup,
+          ctrlInfo.win32?.touch ?? null,
+          ctrlInfo.win32?.key ?? null,
+          ctrlInfo.win32?.screencap ?? null
+        )
+      }
+    }
+
+    const resInfo = data.resource.find(x => x.name === config.resource)
+
+    if (!resInfo) {
+      await vscode.window.showErrorMessage(
+        vscode.l10n.t('maa.pi.error.cannot-find-resource', config.resource)
+      )
+      return null
+    }
+
+    result.resource_path = resInfo.path.map(x => x.replace('{PROJECT_DIR}', root))
+
+    result.task = []
+    for (const task of config.task) {
+      const taskInfo = data.task.find(x => x.name === task.name)
+
+      if (!taskInfo) {
+        await vscode.window.showErrorMessage(
+          vscode.l10n.t('maa.pi.error.cannot-find-task', task.name)
+        )
+        return null
+      }
+
+      const param: {} = {}
+
+      // 是不是该检查一下task里面指定的option是否都被配置了？如果缺失的话看看要不要读下default
+      for (const opt of task.option ?? []) {
+        const optInfo = data.option?.[opt.name]
+
+        if (!optInfo) {
+          await vscode.window.showErrorMessage(
+            vscode.l10n.t('maa.pi.error.cannot-find-option', opt.name)
+          )
+          return null
+        }
+
+        const csInfo = optInfo.cases.find(x => x.name === opt.value)
+
+        if (!csInfo) {
+          await vscode.window.showErrorMessage(
+            vscode.l10n.t('maa.pi.error.cannot-find-case-for-option', opt.value, opt.name)
+          )
+          return null
+        }
+
+        Object.assign(param, csInfo.param ?? {})
+      }
+
+      // 看看FW代码里面这玩意在前面还是后面
+      Object.assign(param, taskInfo.param ?? {})
+
+      result.task.push({
+        name: task.name,
+        entry: taskInfo.entry,
+        param: param
+      })
+    }
+
+    return result as InterfaceRuntime
+  }
+
+  async launchRuntime(runtime: InterfaceRuntime) {
+    let controller: maa.ControllerBase
+
+    if (runtime.controller_param.ctype === 'adb') {
+      controller = new maa.AdbController(runtime.controller_param)
+    } else if (runtime.controller_param.ctype === 'win32') {
+      controller = new maa.Win32Controller(
+        runtime.controller_param.hwnd,
+        runtime.controller_param.controller_type
+      )
+    } else {
+      return
+    }
+
+    controller.notify = (msg, detail) => {
+      this.outputChannel.append(`${msg} ${detail}\n`)
+    }
+
+    await controller.post_connection()
+
+    const resource = new maa.Resource()
+
+    resource.notify = (msg, detail) => {
+      this.outputChannel.append(`${msg} ${detail}\n`)
+    }
+
+    for (const path of runtime.resource_path) {
+      await resource.post_path(path)
+    }
+
+    const instance = new maa.Instance()
+
+    instance.notify = (msg, detail) => {
+      this.outputChannel.append(`${msg} ${detail}\n`)
+    }
+
+    instance.bind(controller)
+    instance.bind(resource)
+
+    if (!instance.inited) {
+      instance.destroy()
+      controller.destroy()
+      resource.destroy()
+      return
+    }
+
+    for (const task of runtime.task) {
+      await instance.post_task(task.entry, task.param as unknown as maa.PipelineDecl).wait()
+    }
   }
 }

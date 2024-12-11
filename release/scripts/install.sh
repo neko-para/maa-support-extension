2bda42476c254e051747d62966d8d4822f89d2ad
nekosu <liao.hy@outlook.com>
2024-12-11 17:21:17 +0800

    fix: package

--- release/scripts/install.sh
+++ release/scripts/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# 不知道为啥会有影响
+npm pkg delete devDependencies.@maaxyz/maa-node
+
 npm i -f @maaxyz/maa-node
+
 for os in win32 linux darwin; do
   for arch in x64 arm64; do
     npm i -f @maaxyz/maa-node-${os}-${arch}

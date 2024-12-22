29a5cac81096c20cd81f7b85b541d9245c7ad8dd
nekosu <liao.hy@outlook.com>
2024-12-22 17:07:55 +0800

    feat: pick color hex

--- pkgs/controlPanel/src/crop/views/VPick.vue
+++ pkgs/controlPanel/src/crop/views/VPick.vue
@@ -7,8 +7,12 @@ import * as pickSt from '@/crop/states/pick'
   <div class="flex gap-1 items-center">
     <vsc-button @click="pickSt.start()"> pick </vsc-button>
     <template v-if="pickSt.color.value">
-      <span class="select-none">
-        picked color: <span class="select-all">{{ pickSt.color.value.join(', ') }}</span>
+      <span class="select-none flex gap-1">
+        picked color:
+        <span class="select-all">{{ pickSt.color.value.join(', ') }}</span>
+        <span class="select-all"
+          >#{{ pickSt.color.value.map(x => x.toString(16).toUpperCase()).join('') }}</span
+        >
       </span>
       <div class="w-6 h-6" :style="`background-color: rgb(${pickSt.color.value.join(',')});`"></div>
     </template>

c2ee4aefccaf8b643fcb5e2fe59ede049e5b974f
nekosu <liao.hy@outlook.com>
2025-09-19 16:41:05 +0800

    chore: add interface v2 types

--- pkgs/types/src/pi_v2.ts
+++ pkgs/types/src/pi_v2.ts
@@ -1,25 +1,17 @@
 import type * as maa from '@maaxyz/maa-node'
 
-export type LocalizedString =
-  | string
-  | {
-      [locale: string]: string
-    }
-
 export type EntryBase = {
-  label?: LocalizedString
-  description?: LocalizedString
+  label?: string
+  description?: string
   icon?: string
 }
 
 export type ControllerBase = EntryBase & {
   name: string
   // 三选一, 从上到下优先
-  resolution?: {
-    short?: number
-    long?: number
-    raw?: true
-  }
+  display_short_side?: number
+  display_long_side?: number
+  display_raw?: boolean
 }
 
 export type AdbController = ControllerBase & {
@@ -60,8 +52,7 @@ export type Task = EntryBase & {
 
 export type SelectCase = EntryBase & {
   name: string
-  // 这个我改了个名字
-  enable?: string[]
+  option?: string[]
   pipeline_override?: unknown
 }
 
@@ -73,22 +64,14 @@ export type SelectOption = EntryBase & {
 
 export type InputItem = EntryBase & {
   name: string
-} & (
-    | {
-        type: 'number'
-        default?: number
-      }
-    | {
-        type: 'string'
-        default?: string
-        verify?: string
-      }
-  )
+
+  default?: string
+  pipeline_type?: 'number' | 'string'
+  verify?: string
+}
 
 export type InputOption = EntryBase & {
   type: 'Input'
-  // 要支持吗?
-  // enable?: string[]
   input: InputItem[]
   pipeline_override?: unknown
 }
@@ -97,9 +80,9 @@ export type Option = SelectOption | InputOption
 
 export type InterfaceV2 = {
   interface_version: 2
-  languages?: string[]
-  name?: LocalizedString // 这里是不是和别的地方统一, 改成label
-  title?: LocalizedString
+  languages?: Record<string, string>
+  name?: string // 这里是不是和别的地方统一, 改成label
+  title?: string
   icon?: string
   mirrorchyan_rid?: string
   mirrorchyan_multiplatform?: boolean
@@ -107,10 +90,10 @@ export type InterfaceV2 = {
   auto_update_maafw?: boolean
   github?: string
   version?: string
-  contact?: LocalizedString
+  contact?: string
   license?: string
-  welcome?: LocalizedString
-  description?: LocalizedString
+  welcome?: string
+  description?: string
 
   // 是不是考虑支持区分平台? 不然只能在action里面改
   agent?: {

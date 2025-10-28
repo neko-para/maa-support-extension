10a2f2c167bd9ad7480e138d6f6c6c031f4508ff
nekosu <liao.hy@outlook.com>
2025-10-28 18:19:10 +0800

    feat: debounce input edit

--- pkgs/webview/src/control/components/TaskInputOption.vue
+++ pkgs/webview/src/control/components/TaskInputOption.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { NButton, NFlex, NInput } from 'naive-ui'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
-import type { InputOption, TaskConfig } from '@mse/types'
+import type { InputItem, InputOption, TaskConfig } from '@mse/types'
 
 import { t } from '../../utils/locale'
 import { ipc } from '../ipc'
@@ -43,19 +43,95 @@ function revealInput(name: string) {
   })
 }
 
+function validate(info: InputItem, value: string | undefined): undefined | 'error' {
+  if (!info.verify) {
+    return undefined
+  }
+  const re = new RegExp(info.verify)
+  if (re.test(value ?? '')) {
+    return undefined
+  } else {
+    return 'error'
+  }
+}
+
+const inputCache = ref<Record<string, string | null>>({})
+
+let prevKey: null | string = null
+let prevConfig: null | (() => void) = null
+let prevTimer: null | number = null
+
+function flushDifferent(key: string) {
+  if (prevKey !== null && prevKey !== key) {
+    const oldKey = prevKey
+    prevConfig?.()
+    setTimeout(() => {
+      delete inputCache.value[oldKey]
+    }, 10)
+  }
+  if (prevTimer !== null) {
+    clearTimeout(prevTimer)
+  }
+
+  prevKey = null
+  prevConfig = null
+  prevTimer = null
+}
+
 function configTask(option: string, key: string, value: string) {
   if (!props.task.__vscKey) {
     return
   }
+  const taskKey = props.task.__vscKey
+
+  flushDifferent(key)
+
+  prevKey = key
+  prevConfig = () => {
+    ipc.send({
+      command: 'configTask',
+      key: taskKey,
+      option,
+      value: {
+        ...optValue.value,
+        [key]: value
+      }
+    })
+    setTimeout(() => {
+      delete inputCache.value[key]
+    }, 10)
+  }
+  prevTimer = setTimeout(() => {
+    prevConfig?.()
+
+    prevKey = null
+    prevConfig = null
+    prevTimer = null
+  }, 500) as unknown as number
+  inputCache.value[key] = value
+}
+
+function configTaskRemove(option: string, key: string) {
+  if (!props.task.__vscKey) {
+    return
+  }
+  const taskKey = props.task.__vscKey
+
+  flushDifferent(key)
+
+  const vals = {
+    ...optValue.value
+  }
+  delete vals[key]
   ipc.send({
     command: 'configTask',
-    key: props.task.__vscKey,
+    key: taskKey,
     option,
-    value: {
-      ...optValue.value,
-      [key]: value
-    }
+    value: vals
   })
+  setTimeout(() => {
+    delete inputCache.value[key]
+  }, 10)
 }
 </script>
 
@@ -67,15 +143,25 @@ function configTask(option: string, key: string, value: string) {
     <n-flex>
       <n-button @click="revealInput(info.name)" text> {{ info.name }} </n-button>
     </n-flex>
-    <n-input
-      :value="optValue?.[info.name] ?? null"
-      @update:value="
-        value => {
-          configTask(opt, info.name, value)
-        }
-      "
-      :placeholder="info.default"
-      size="small"
-    ></n-input>
+    <n-flex :wrap="false">
+      <n-input
+        :value="inputCache[info.name] ?? optValue?.[info.name] ?? null"
+        :status="validate(info, optValue?.[info.name] ?? info.default)"
+        @update:value="
+          value => {
+            configTask(opt, info.name, value)
+          }
+        "
+        :placeholder="optValue?.[info.name] === undefined ? info.default : ''"
+        size="small"
+      ></n-input>
+      <n-button
+        v-if="optValue?.[info.name] !== undefined"
+        @click="configTaskRemove(opt, info.name)"
+        text
+      >
+        reset
+      </n-button>
+    </n-flex>
   </template>
 </template>

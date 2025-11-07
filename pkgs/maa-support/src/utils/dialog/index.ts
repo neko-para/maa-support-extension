365d51f414b63f9f74c897cfdeffe29db342a7bf
nekosu <liao.hy@outlook.com>
2025-11-08 00:11:14 +0800

    feat: file selection for win

--- pkgs/maa-support/src/utils/dialog/index.ts
+++ pkgs/maa-support/src/utils/dialog/index.ts
@@ -1,9 +1,12 @@
 import * as macImpl from './mac'
+import * as winImpl from './win'
 
 export async function showFolderDialog(prompt: string) {
   switch (process.platform) {
     case 'darwin':
       return await macImpl.showFolderDialog(prompt)
+    case 'win32':
+      return await winImpl.showFolderDialog(prompt)
     default:
       return null
   }
@@ -13,6 +16,8 @@ export async function showOpenFileDialog(prompt: string) {
   switch (process.platform) {
     case 'darwin':
       return await macImpl.showOpenFileDialog(prompt)
+    case 'win32':
+      return await winImpl.showOpenFileDialog(prompt)
     default:
       return null
   }
@@ -22,7 +27,13 @@ export async function showSaveFileDialog(prompt: string, filename: string) {
   switch (process.platform) {
     case 'darwin':
       return await macImpl.showSaveFileDialog(prompt, filename)
+    case 'win32':
+      return await winImpl.showSaveFileDialog(prompt, filename)
     default:
       return null
   }
 }
+
+showFolderDialog('123123').then(res => {
+  console.log(res)
+})

f4ff35ea485d16094461de907b1ffb25fb60426b
nekosu <liao.hy@outlook.com>
2025-12-03 01:04:11 +0800

    fix: csp

--- pkgs/utils/src/webview/data.ts
+++ pkgs/utils/src/webview/data.ts
@@ -1,4 +1,4 @@
 export const cspMeta = `<meta
   http-equiv="Content-Security-Policy"
-  content="default-src 'none'; font-src %{cspSource}; style-src 'unsafe-inline' %{cspSource}; script-src %{cspSource}; img-src %{cspSource} data:; connect-src %{cspSource} data:;"
+  content="default-src 'none'; font-src %{cspSource}; style-src 'unsafe-inline' %{cspSource}; script-src 'unsafe-eval' %{cspSource}; img-src %{cspSource} data:; connect-src %{cspSource} data:;"
 />`

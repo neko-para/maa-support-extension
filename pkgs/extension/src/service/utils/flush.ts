0b6756f4db5f73674bd2ad1af683ddcbcd2495e2
nekosu <liao.hy@outlook.com>
2026-01-23 21:24:18 +0800

    chore: remove useless code

--- pkgs/extension/src/service/utils/flush.ts
+++ pkgs/extension/src/service/utils/flush.ts
@@ -1,6 +1,4 @@
-import * as vscode from 'vscode'
-
-import { DisposableHelper, context } from '../context'
+import { DisposableHelper } from '../context'
 
 export abstract class FlushHelper extends DisposableHelper {
   abstract doFlush(): Promise<void>
@@ -35,53 +33,5 @@ export abstract class FlushHelper extends DisposableHelper {
         this.flushDirty()
       }, 0)
     }
-  }
-}
-
-export abstract class FSWatchFlushHelper extends FlushHelper {
-  abstract doUpdate(dirtyPath: string[]): Promise<void>
-
-  watcher: vscode.FileSystemWatcher
-
-  dirtyPaths: Set<string>
-
-  constructor(pattern: vscode.GlobPattern, tester: (uri: vscode.Uri) => boolean) {
-    super()
-
-    this.watcher = vscode.workspace.createFileSystemWatcher(pattern)
-    context.subscriptions.push(this.watcher)
-
-    this.dirtyPaths = new Set()
-
-    this.defer = vscode.workspace.onDidChangeTextDocument(event => {
-      if (tester(event.document.uri)) {
-        this.dirtyPaths.add(event.document.uri.fsPath)
-      }
-    })
-
-    this.watcher.onDidCreate(uri => {
-      if (tester(uri)) {
-        this.dirtyPaths.add(uri.fsPath)
-      }
-    })
-
-    this.watcher.onDidDelete(uri => {
-      if (tester(uri)) {
-        this.dirtyPaths.add(uri.fsPath)
-      }
-    })
-
-    this.watcher.onDidChange(uri => {
-      if (tester(uri)) {
-        this.dirtyPaths.add(uri.fsPath)
-      }
-    })
-  }
-
-  async doFlush() {
-    const paths = [...this.dirtyPaths]
-    this.dirtyPaths = new Set()
-
-    await this.doUpdate(paths)
   }
 }

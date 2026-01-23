5bc5b8bf1e2bbe8abf1e840ba8d72b979e0a5be8
nekosu <liao.hy@outlook.com>
2026-01-23 19:28:59 +0800

    chore: update fw

--- pkgs/extension/src/service/native.ts
+++ pkgs/extension/src/service/native.ts
@@ -23,7 +23,7 @@ function isValidRegistryType(key: unknown): key is keyof typeof registries {
 const defaultRegistryType = 'npm'
 
 const defaultMaaVersion = packageJson.devDependencies['@maaxyz/maa-node']
-const minimumMaaVersion = '5.5.0-beta.1'
+const minimumMaaVersion = '5.5.0-beta.3'
 
 function fixMinimumVersion(ver: string) {
   if (semVerCompare(ver, minimumMaaVersion) === -1) {

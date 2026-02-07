5db8e455c6b272d808f25f488717199ff145536d
nekosu <liao.hy@outlook.com>
2026-02-07 20:39:45 +0800

    feat: extends maximum length of brief task name
    fix #33

--- pkgs/webview/src/control/utils.ts
+++ pkgs/webview/src/control/utils.ts
@@ -1,4 +1,4 @@
-export function makeBrief(str: string, maxLen = 10, side = 4) {
+export function makeBrief(str: string, maxLen = 20, side = 8) {
   if (str.length > maxLen) {
     str = str.substring(0, side) + '..' + str.substring(str.length - side)
   }

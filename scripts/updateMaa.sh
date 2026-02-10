f0535d5ac036bdca459f286c456d4436dde72fe9
nekosu <liao.hy@outlook.com>
2026-02-10 12:34:51 +0800

    fix: maa version

--- scripts/updateMaa.sh
+++ scripts/updateMaa.sh
@@ -2,7 +2,7 @@
 
 ver=`cat scripts/.maaver`
 
-for dir in webview extension types; do
+for dir in extension maa-checker maa-server maa-server-proto pipeline-manager types webview; do
   cd pkgs/$dir
   pnpm i -D @maaxyz/maa-node@$ver
   cd -
